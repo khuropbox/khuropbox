9569a022c78fc53cdbeed8e6161b366cfa1dace1
graykode <nlkey2022@gmail.com>
2019-04-16 17:07:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+## KHUropbox
+
+<p align="center"><img width="200" src="static/image/header.png" /></p>
+
+클라우드 플랫폼을 사용한 2019년 1학기 클라우드 컴퓨팅 과목 팀 프로젝트입니다. 팀원 들의 원할한 소통을 위해 영어보다 한국어로 작성되었습니다.
+
+
+
+##### Notice
+
+- 새로운 기능 구현 시 branch로 관리 혹은 fork에서 자신 repository에서 구현 후 Pull Request로 합쳐주세요.
+- 버그, Trouble Shooting은 Issue를 활용해주세요.
+
+
+
+##### 구현 목록(및 예정)
+
+- [x] 로그인, 회원가입  
+
+- [ ] 비밀번호 수정
+
+- [ ] S3 기반 파일 업로드, 삭제
+
+- [ ] AWS 플랫폼 DB 기반(MySQL 예정) migration 연동
+
+- [ ] 외부인과 파일 공유, Session Problem
+
+- [ ] 도커 가상화(Dockerfile) 예정
+
+- [ ] Elastic Beanstalk와 Elastic Load Balancing을 사용한 부하분산처리
+
+  
+
+**추후에 AWS 이용할 때 AWS 키 안올리도록 주의해주세요.**
+
+
+
+## Quick Start
+
+```shell
+$ git clone https://github.com/khuropbox/khuropbox && cd khuropbox
+$ pip pip install -r requirements.txt
+
+$ python manage.py migrate
+$ python manage.py runserver
+```
+
+
+
+## Dependancy
+
+- django==2.1.7
+- Pillow==5.4.1
+- djangorestframework==3.9.2
+
+
+
+## Team Members
+
+- 정다은
+- 정의동
+- 정태환(@graykode)
+- 조아진
---
+++ khuropbox/views.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.core.exceptions import PermissionDenied
+
+import django
+
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, "index.html")
+    else:
+        return render(request, "login.html")
+
+def login(request):
+    if request.user.is_authenticated:
+        raise PermissionDenied
+    else:
+        if request.method == "POST":
+            if not all(i in request.POST for i in ('username', 'password')):
+                return render(request, "login.html", {
+                    "message": "아이디와 비밀번호를 입력 해 주세요"
+                })
+            user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user is not None:
+                auth = django.contrib.auth.login(request, user)
+                return redirect('/')
+            else:
+                return render(request, "login.html", {
+                    "message": "아이디와 비밀번호를 확인해 주세요"
+                })
+        else:
+            return render(request, "login.html")
+
+def logout(request):
+    if request.user.is_authenticated:
+        django.contrib.auth.logout(request)
+    return redirect("/")
+
+def register(request):
+    if request.user.is_authenticated: raise PermissionDenied
+    if request.method == "POST":
+        require_keys = ('username', 'password', 'first_name', 'last_name', 'email')
+        if all(i in request.POST for i in require_keys):
+            if User.objects.filter(username=request.POST['username']).count():
+                return render(request, 'register.html', {
+                    "message": '이미 존재하는 아이디입니다.'
+                })
+            if User.objects.filter(username=request.POST['email']).count():
+                return render(request, 'register.html', {
+                    "message": '이미 존재하는 이메일입니다.'
+                })
+
+            userobj = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password'],
+                first_name=request.POST['first_name'],
+                last_name=request.POST['last_name'],
+                email=request.POST['email']
+            )
+            return render(request, 'index.html')
+        else:
+            return render(request, 'register.html', {
+                "message": '모든 항목을 입력해주세요.'
+            })
+    else:
+        return render(request, 'register.html')
+
