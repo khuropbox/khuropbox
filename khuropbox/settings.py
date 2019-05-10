6b636a643e1e4e729a2ed69a60074248ef6bc802
graykode <nlkey2022@gmail.com>
2019-05-10 18:54:18 +0900

    local DB를 사용하기 위해 dev branch 설정

--- khuropbox/settings.py
+++ khuropbox/settings.py
@@ -86,14 +86,17 @@ WSGI_APPLICATION = 'khuropbox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': config['db']['ENGINE'],
-        'HOST': config['db']['HOST'],
-        'PORT': config['db']['PORT'],
-        'NAME': config['db']['NAME'],
-        'USER': config['db']['USER'],
-        'PASSWORD': config['db']['PASSWORD'],
+        # 'ENGINE': config['db']['ENGINE'],
+        # 'HOST': config['db']['HOST'],
+        # 'PORT': config['db']['PORT'],
+        # 'NAME': config['db']['NAME'],
+        # 'USER': config['db']['USER'],
+        # 'PASSWORD': config['db']['PASSWORD'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
