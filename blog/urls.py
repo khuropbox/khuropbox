00504751c2c076dbf35862bf1934aef0fc1c117c
graykode <nlkey2022@gmail.com>
2019-05-14 03:14:05 +0900

    이전 blog의 템플릿을 메인 template으로 옮김

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
-    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
-    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
-    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    path('', views.list, name='list'),
+    path('post/<int:pk>/', views.detail, name='detail'),
+    path('post/new/', views.new, name='new'),
+    path('post/<int:pk>/edit/', views.edit, name='edit'),
+    url(r'^drafts/$', views.commentlist, name='commentlist'),
+
+    url(r'^post/(?P<pk>\d+)/remove/$', views.remove, name='remove'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.commentadd, name='commentadd'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404, redirect
@@ -6,29 +5,28 @@ from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+def list(request):
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'postlist.html', {'posts': posts})
 
-def post_detail(request, pk):
+def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'postdetail.html', {'post': post})
 
 @login_required
-def post_new(request):
+def new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+            return redirect('/blog/')
+
+    return render(request, 'postedit.html')
 
 @login_required
-def post_edit(request, pk):
+def edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
@@ -36,36 +34,30 @@ def post_edit(request, pk):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+            return redirect('/blog/')
+
+    return render(request, 'postedit.html', {'post' : post})
 
 @login_required
-def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+def commentlist(request):
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'commentadd.html', {'posts': posts})
 
 @login_required
-def post_publish(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.publish()
-    return redirect('post_detail', pk=pk)
-
-def post_remove(request, pk):
+def remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
-    return redirect('post_list')
+    return redirect('/blog/')
 
-def add_comment_to_post(request, pk):
+@login_required
+def commentadd(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
+        print(form)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+            return redirect('/blog/post/')
+    return render(request, 'commentadd.html')
