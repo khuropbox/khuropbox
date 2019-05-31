2b5f8054c0a0c9f62f765f3aef2c4f6d8f724c20
graykode <nlkey2022@gmail.com>
2019-05-31 17:51:26 +0900

    사용자 마다 Bucket의 폴더 나누기, 회원가입시 고유 folder 생성

--- djangoS3Browser/templatetags/s3-tags.py
+++ djangoS3Browser/templatetags/s3-tags.py
@@ -3,12 +3,11 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("index.html")
-def load_s3():
-    return {}
-
+@register.inclusion_tag("index.html", takes_context=True)
+def load_s3(context):
+    request = context['request']
+    return {'root' : '-' + str(request.user.username) + '/' }
 
 @register.inclusion_tag("header.html")
 def load_s3_header():
     return {}
-
